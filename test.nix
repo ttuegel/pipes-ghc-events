76b60373d69bd4377a397f6fbfb3cf9bb303bc15
Thomas Tuegel <ttuegel@mailbox.org>
2022-04-01 09:56:13 -0500

    Add GitHub Actions workflow

---
+++ test.nix
@@ -0,0 +1,8 @@
+let 
+  default = import ./. {};
+  inherit (default) project;
+in
+[
+  project.pipes-ghc-events.components.library
+  project.eventlog2speedscope.components.exes.eventlog2speedscope
+]
